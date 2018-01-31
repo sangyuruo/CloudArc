edc2b77ed85c9dd29db81e13a566ec15ec3e7e34
youhong  <youhong@hxxn.onaliyun.com>
2018-01-31 14:22:51 +0800

    add..

--- src/main/java/com/emcloud/arc/analysis/domain/TemperatureHumidity.java
+++ src/main/java/com/emcloud/arc/analysis/domain/TemperatureHumidity.java
@@ -6,4 +6,20 @@ import com.emcloud.arc.analysis.impl.TemperatureAnalysis;
 public class TemperatureHumidity {
       private HumidityAnalysis humidityAnalysis;
       private TemperatureAnalysis temperatureAnalysis;
+
+    public HumidityAnalysis getHumidityAnalysis() {
+        return humidityAnalysis;
+    }
+
+    public void setHumidityAnalysis(HumidityAnalysis humidityAnalysis) {
+        this.humidityAnalysis = humidityAnalysis;
+    }
+
+    public TemperatureAnalysis getTemperatureAnalysis() {
+        return temperatureAnalysis;
+    }
+
+    public void setTemperatureAnalysis(TemperatureAnalysis temperatureAnalysis) {
+        this.temperatureAnalysis = temperatureAnalysis;
+    }
 }
--- src/main/java/com/emcloud/arc/analysis/impl/ReactivePowerAnalysis.java
+++ src/main/java/com/emcloud/arc/analysis/impl/ReactivePowerAnalysis.java
@@ -1,4 +1,26 @@
 package com.emcloud.arc.analysis.impl;
 
-public class ReactivePowerAnalysis {
+import com.emcloud.arc.analysis.analysis.DefaultAnalysisResult;
+import com.emcloud.arc.analysis.analysis.DefaultOneParamAnalysis;
+
+public class ReactivePowerAnalysis extends DefaultOneParamAnalysis {
+    @Override
+    public DefaultAnalysisResult handle(String reactivePowerStr) {
+        DefaultAnalysisResult defaultAnalysis = new DefaultAnalysisResult();
+        int reactivePower = Integer.parseInt(reactivePowerStr);
+        int alarmLevel = 0;
+        boolean alarm = true;
+        if (reactivePower > 10 && reactivePower < 20) {
+            alarmLevel = 1;
+        } else if (reactivePower >= 20 && reactivePower < 30) {
+            alarmLevel = 2;
+        } else if (reactivePower >= 30) {
+            alarmLevel = 3;
+        }else{
+            alarm = false;
+        }
+        defaultAnalysis.setAlarm( alarm );
+        defaultAnalysis.setAlarmLevel(alarmLevel);
+        return defaultAnalysis;
+    }
 }
